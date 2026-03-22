b8c402b4c53c6b1567096185cb488fabfcad150b
Afzal14786 <mdafzal14777@gmail.com>
2026-03-23 03:15:19 +0530

    remove .env from github and fix port

--- src/api/api.index.ts
+++ src/api/api.index.ts
@@ -1,7 +1,7 @@
 import axios, { type InternalAxiosRequestConfig, AxiosError } from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1",
   withCredentials: true, // Crucial for OAuth cookies if used
 });
 
